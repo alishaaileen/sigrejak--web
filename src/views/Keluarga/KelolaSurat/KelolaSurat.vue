6570c921669eee9487ebb267d06f0c9aa7c63998
alishaileen <alishaileen05@gmail.com>
2021-03-30 19:29:47 +0700

    added many pages and added get profile

---
+++ src/views/Keluarga/KelolaSurat/KelolaSurat.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container>
+    <div class="d-flex flex-wrap">
+      <div v-for="(card, i) in cards" :key="i">
+        <router-link :to="card.to" class="text-decoration-none">
+          <v-card
+            class="sm mb-4 mr-4 align-stretch"
+            color="blue-grey lighten-4"
+            outlined
+            width="500"
+            hover
+            tile
+          >
+            <div>
+              <v-card-title
+                class="headline text-decoration-none"
+                v-text="card.title"
+              ></v-card-title>
+
+              <v-card-subtitle v-text="card.desc"></v-card-subtitle>
+            </div>
+          </v-card>
+        </router-link>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+data: () => ({
+    cards: [
+      {
+        title: 'Surat Keterangan Pindah',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-keterangan-pindah',
+      },
+      {
+        title: 'Surat Keterangan Mati',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-keterangan-mati',
+      },
+      {
+        title: 'Surat Pelayanan Minyak Suci',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-pelayanan-minyak-suci',
+      },
+      {
+        title: 'Surat Keterangan Calon Pengantin',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-keterangan-calon-pengantin',
+      },
+      {
+        title: 'Surat Keterangan',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-keterangan',
+      },
+      {
+        title: 'Surat Baptis Anak',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-baptis-anak',
+      },
+      {
+        title: 'Surat Baptis Dewasa',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-baptis-dewasa',
+      },
+      {
+        title: 'Surat Komuni',
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-komuni',
+      },
+      {
+        title: 'Surat Keterangan Beasiswa', 
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-keterangan-beasiswa',
+      },
+      {
+        title: 'Surat Izin Pelayanan Ekaristi', 
+        desc: 'lorem ipsum',
+        to: 'kelola-surat/surat-izin-pelayanan-ekaristi',
+      },
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
