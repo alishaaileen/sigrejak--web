862098773bb03ab1bd0d2069f453d9ac3f92814a
alishaileen <alishaileen05@gmail.com>
2021-07-25 07:13:11 +0700

    added badge to surat keterangan pengurus

--- src/views/Pengurus/KelolaSurat/index.vue
+++ src/views/Pengurus/KelolaSurat/index.vue
@@ -21,7 +21,15 @@
           >
             <div>
               <v-card-title class="headline">
-                <h5>{{ card.title }}</h5>
+                <v-badge
+                lar
+                  color="amber darken-1"
+                  :value="card.unverifiedCount > 0"
+                  :content="card.unverifiedCount"
+                  class="mb-2"
+                >
+                  <h3>{{ card.title }}</h3>
+                </v-badge>
               </v-card-title>
 
               <v-card-subtitle v-text="card.desc"></v-card-subtitle>
@@ -34,59 +42,80 @@
 </template>
 
 <script>
+import { getData } from '../../../utils'
 export default {
   data: () => ({
     search: '',
     cards: [
       {
+        endpoint: 'surat-izin-pelayanan-ekaristi',
         title: 'Surat Izin Pelayanan Ekaristi', 
         desc: 'Pelayanan ekaristi yang dipimpin oleh Romo dari luar Paroki Kumetiran dicatat dalam surat ini',
         to: 'surat/surat-izin-ekaristi',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-keterangan-beasiswa',
         title: 'Surat Keterangan Beasiswa', 
         desc: 'Pengajuan beasiswa paroki bagi warga Paroki Kumetiran',
         to: 'surat/surat-keterangan-beasiswa',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-keterangan',
         title: 'Surat Keterangan',
         desc: 'Surat yang dapat dipakai untuk bermacam keperluan selain keterangan perkawinan',
         to: 'surat/surat-keterangan',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-baptis-anak',
         title: 'Surat Baptis Anak',
         desc: 'Pengajuan baptisan anak yang berusia 1 hari sampai 7 tahun',
         to: 'surat/surat-baptis-anak',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-baptis-dewasa',
         title: 'Surat Baptis Dewasa',
         desc: 'Pengajuan baptisan bagi orang yang berusia diatas 7 tahun',
         to: 'surat/surat-baptis-dewasa',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-komuni-penguatan',
         title: 'Surat Komuni/Penguatan (Krisma)',
         desc: 'Pengajuan komuni pertama dan/atau penguatan bagi umat yang sudah menerima baptis Katolik',
         to: 'surat/surat-komuni-penguatan',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-keterangan-calon-pengantin',
         title: 'Surat Keterangan Calon Pengantin',
         desc: 'Surat yang dibuat ketika umat Paroki Kumetiran akan menikah',
         to: 'surat/surat-keterangan-calon-pengantin',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-pelayanan-minyak-suci',
         title: 'Surat Pelayanan Minyak Suci',
         desc: 'Surat yang menampung informasi pelayanan minyak suci yang ada di wilayah Paroki Kumetiran',
         to: 'surat/surat-minyak-suci',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-keterangan-mati',
         title: 'Surat Keterangan Mati',
         desc: 'Surat yang dibuat jika ada umat Kumetiran yang meninggal dunia',
         to: 'surat/surat-keterangan-mati',
+        unverifiedCount: 0,
       },
       {
+        endpoint: 'surat-keterangan-pindah',
         title: 'Surat Keterangan Pindah',
         desc: 'Umat Paroki Kumetiran yang akan pindah ke paroki lain harus membuat surat ini',
         to: 'surat/surat-keterangan-pindah',
+        unverifiedCount: 0,
       },
     ],
   }),
@@ -97,8 +126,18 @@ export default {
       })
     },
   },
+  mounted() {
+    this.getSuratNotApprovedByKetuaLingkungan()
+  },
   methods: {
-    
+    getSuratNotApprovedByKetuaLingkungan() {
+      this.cards.map(async (card) => {
+        let tempData = await getData(`/${card.endpoint}`)
+        
+        card.unverifiedCount = tempData.filter(surat => surat.sekretariat_approval != 1 && surat.ketua_lingkungan_approval === 1)
+        card.unverifiedCount = card.unverifiedCount.length
+      })
+    }
   }
 }
 </script>
